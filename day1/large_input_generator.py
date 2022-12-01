2549f2deeb1beab7b8e4d5ed54abefab07fd7302
startwarfields <j.warfieldmeyer@gmail.com>
2022-11-30 22:33:19 -0800

    Add large input generator for benchmarking & move folder.

---
+++ day1/large_input_generator.py
@@ -0,0 +1,25 @@
+import argparse
+import numpy as np
+
+def generate_input_dataset(num_elves):
+    with open('large_input.txt', 'w') as f:
+        for i in range(num_elves):
+            elf_sz = np.random.randint(1, 15)
+            items = generate_random_numpy_array(elf_sz)
+            for j in items:
+                f.write(str(j) + '\n')
+            f.write('\n')
+    
+
+def generate_random_numpy_array(size):
+    return np.random.randint(1000, 100000, size)
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog = "Day 1 Advent of Code 2022: Challenge",
+                                    description = "Extend the first day of Advent of Code 2022 with MIPS Scale Elves" )
+    parser.add_argument('-n', '--num_elves', help = 'Number of elves', required = False, default = 1_000_000)
+
+    args = parser.parse_args()
+    generate_input_dataset(args.num_elves)
--- day1/main.py
+++ day1/main.py
@@ -1,26 +1,34 @@
 import argparse
+import time
 def get_max_calorie_elf(elf_list):
     current_elf = 0
     elves = dict()
+    num_snacks = 0
     for line in elf_list:
         if line == '\n':
             current_elf += 1
         else:
+            num_snacks += 1
             if current_elf not in elves:
                 elves[current_elf] = 0
             elves[current_elf] += int(line)
+    print("Number of snacks: {}".format(num_snacks))
     return sorted(elves.values(), reverse=True)
 
 
 def main(args):
 
     elf_list = open(args.input, 'r')
+    t1 = time.time()
     max_calories = get_max_calorie_elf(elf_list)
+    t2 = time.time()
     elf_list.close()
     
     # print("The max calories is: {}".format(max_calories))
+    print("Total time: {}".format(t2-t1))
     print("The top {} elves are: {}".format(args.num_top_elves, max_calories[:args.num_top_elves]))
     print("The total sum is {}".format(sum(max_calories[:args.num_top_elves])))
+    print("The smallest elf is {}".format(max_calories[-1]))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog = "Day 1 Advent of Code 2022",
